13e1c08f9481e8b75c72bcb3bae4d8f91b7d7947
Steve Baker <steve@smigg.co.uk>
2020-06-14 22:16:11 +0100

    updated RAG method, questions object creation and framework for free text and frequency tables methods

--- platform_test.py
+++ platform_test.py
@@ -16,8 +16,22 @@ def create_survey():
 # sp.get_heatmap(survey.reporting, ['Q2', 'Q3', 'Q4'])
 def create_rags():
     for org in survey.sample.get_orgs():
-        sp.get_rag(survey, seperator=org, group_by=['locality1', 'locality2'], questions=survey.questions)
+        sp.get_rag(survey, seperator=org, group_by=['locality1'], questions=survey.questions)
+        break
+
+
+def create_freq():
+    for org in survey.sample.get_orgs():
+        sp.get_freq_tables(org, survey.combined.df, survey.questions)
+        break
+
+def create_freetext():
+    for org in survey.sample.get_orgs():
+        sp.get_freetext(org, survey.combined.df, survey.questions)
+        break
 
 
 print(timeit.timeit(create_survey, number=repeats)/repeats)
-print(timeit.timeit(create_rags, number=repeats)/repeats)
+#print(timeit.timeit(create_rags, number=repeats)/repeats)
+#print(timeit.timeit(create_freq, number=repeats)/repeats)
+print(timeit.timeit(create_freetext, number=repeats)/repeats)
--- survey_platform/survey_platform.py
+++ survey_platform/survey_platform.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import xlsxwriter
 
 output_path = Path('C:/Users/steve.baker/PycharmProjects/survey_app/survey_app/static/outputs')
 
@@ -14,7 +15,7 @@ scoring_terms = {'pos': {'value': 1, 'string': 'Positive', 'suffix': '_pos'},
 ignore_value = scoring_terms['ignore']['value']
 scored_terms = [i['value'] for i in scoring_terms.values()]
 scored_terms.remove(ignore_value)
-
+suppression_threshold = 1000
 
 def _input_filetype(path, index_col):
     extension = path.split('.')[-1]
@@ -26,21 +27,26 @@ def _input_filetype(path, index_col):
 
 def calc_scores(df, questions, score_types=['pos']):
     for question in questions.get_scored_questions():
-        question_score_response_dict = (questions.questionlist[question]).score_responses
+        question_score_response_dict = question.score_responses
 
         for score in score_types:
-            score_column_header = question + scoring_terms[score]['suffix']
+            score_column_header = question.qid + scoring_terms[score]['suffix']
             score_responses = question_score_response_dict[score]
 
-            df.loc[df[question].isin(scored_terms), score_column_header] = 0
-            df.loc[df[question].isin(score_responses), score_column_header] = 100
+            df.loc[df[question.qid].isin(scored_terms), score_column_header] = 0
+            df.loc[df[question.qid].isin(score_responses), score_column_header] = 100
 
     return df
 
 
-def get_rag(source, seperate_by_field='trust_id', seperator=None, group_by=None, questions=None,
-                    score_type='pos'):
+def get_rag(source,
+            seperate_by_field='trust_id',
+            seperator=None,
+            group_by=None,
+            questions=None,
+            score_type='pos'):
 
+    #check for single layer or multi group by
     if isinstance(group_by, str):
         columns_for_scoring, columns_output = [group_by], [group_by]
         number_groups = 1
@@ -48,10 +54,15 @@ def get_rag(source, seperate_by_field='trust_id', seperator=None, group_by=None,
         columns_for_scoring, columns_output = group_by.copy(), group_by.copy()
         number_groups = len(group_by)
 
+    #iteratre trough questions to select columns and theme mapping
+    topic_dict = {}
     for question in questions.get_scored_questions():
-        columns_for_scoring.append(question)
-        columns_output.append(question + scoring_terms[score_type]['suffix'])
+        topic_dict[question.qid + scoring_terms[score_type]['suffix']] = questions.questions_dict[question.qid].theme
 
+        columns_for_scoring.append(question.qid)
+        columns_output.append(question.qid + scoring_terms[score_type]['suffix'])
+
+    # check type of input, either reporting, survey or just a df
     if isinstance(source, Reporting):
         df = (source.df[source.df[seperate_by_field] == seperator])[columns_output]
     elif isinstance(source, Survey):
@@ -60,28 +71,57 @@ def get_rag(source, seperate_by_field='trust_id', seperator=None, group_by=None,
         df = (source[source[seperate_by_field] == seperator])[columns_for_scoring]
         df = calc_scores(df, questions, [score_type])[columns_output]
 
-    # group by group_variable
-    grouped = df.groupby(group_by).mean()
 
-    # comparator
-    comparator = df[columns_output].mean().to_frame().transpose()
+    #unpivot data
+    melteddf = df.melt(id_vars=group_by)
 
-    if number_groups > 1:
-        multiindex = tuple(['  ' for i in range(number_groups-1)] + ['Organisation'])
-        index = pd.MultiIndex.from_tuples([multiindex])
-    elif number_groups == 1:
-        index = pd.Index(['Organisation'])
+    #map on the question topics
+    melteddf['topic'] = melteddf['variable'].map(topic_dict)
 
-    comparator = comparator.set_index(index)
+    #main frame
+    df_main = melteddf.pivot_table(index=['topic', 'variable'],
+                                     columns=group_by ,
+                                     values='value',
+                                     aggfunc=['count', np.sum],
+                                     margins=True)
 
-    #combined
-    combined = comparator.append(grouped)
+    #by theme subtotals
+    df_theme = df_main.groupby('topic').sum().drop('All') # no need for all on both when combining
+    df_theme['newcolumn'] = df_theme.index #for alphabetical sorting
+    df_theme = df_theme.set_index('newcolumn', append=True)
+    #append
+    hello = df_main.append(df_theme).sort_index()
 
-    # append and output to csv
-    combined.transpose().to_excel(output_path / f'{seperator}_output.xlsx')
+    #calculate mean
+    hi = hello['sum']/hello['count']
 
-    return output_path / f'{seperator}_output.xlsx'
+    # suppression
+    suppression_mask = hello['count'] < suppression_threshold
+    hi = hi.mask(suppression_mask)
 
+    # write to excel
+    writer = pd.ExcelWriter(output_path/f'{seperator}_RAG.xlsx', engine='xlsxwriter')
+    hi.replace(np.nan, '*').to_excel(writer, sheet_name='Sheet1')
+    worksheet = writer.sheets['Sheet1']
+    writer.save()
+
+    return output_path / f'{seperator}_RAG.xlsx'
+
+def get_freq_tables(trust, combined, questions):
+    df = combined[combined['trust_id'] == trust]
+
+    for question in questions.single_response_questions:
+        print(df[question.qid].value_counts().sort_index())
+
+    #currently does not include vlaues which have no responses...
+
+def get_freetext(trust, combined, questions):
+    df = combined[combined['trust_id'] == trust]
+
+    for question in questions.text_response_questions:
+        print(df[question.qid].dropna().reset_index(drop=True))
+
+    # currently does not include vlaues which have no responses...
 
 def get_heatmap(reporting, questions):
     df = reporting.df[questions].replace({1: -2, 2: -1, 3: 0, 4: 1, 5: 2, 6: np.nan})
@@ -106,7 +146,7 @@ class Survey:
         self.sample = Sample(sample_path)
         self.responses = Responses(responses_path)
         self.combined = Combined(self.sample, self.responses, self.questions)
-        self.reporting = Reporting(self.combined, self.questions)
+        #self.reporting = Reporting(self.combined, self.questions)
 
 
 class Sample:
@@ -128,39 +168,66 @@ class Combined:
 
     def __init__(self, sample, responses, questions):
         self.df = sample.df.join(responses.df, how='left')
-        self.questions = questions
 
 
 class Questions:
 
     def __init__(self, question_path):
-        self.df = pd.read_csv(question_path, index_col='qid')
-        self.questionlist = self._generate_dict()
-        self.scored_questions = [k for k, v in self.questionlist.items() if v.pos_scored == 1]
+        self.df = pd.read_csv(question_path)
+        self.theme_dict = {}
+        self.questions_dict = {}
+        self.scored_questions = []
+        self.single_response_questions = []
+        self.multi_response_questions = []
+        self.text_response_questions = []
+        self._generate_question_lists()
 
     def __iter__(self):
-        return iter(self.questionlist.values())
+        return iter(self.questions_dict.values())
 
     def __len__(self):
-        return len(self.questionlist)
+        return len(self.questions_dict)
 
     def get_scored_questions(self):
         return self.scored_questions
 
-    def _generate_dict(self):
+    def _generate_question_lists(self):
         score_indices = [i for i, elem in enumerate(list(self.df.columns)) if 's_' in elem[0:2]]
         response_indices = [i for i, elem in enumerate(list(self.df.columns)) if 'r_' in elem[0:2]]
 
-        return {row[0]: Question(row, score_indices, response_indices) for row in self.df.iterrows()}
+        for row in self.df.iterrows():
+            question_object = Question(row, score_indices, response_indices)
 
+            self.questions_dict[row[1]['qid']] = question_object
+
+            if row[1]['theme'] in self.theme_dict.keys():
+                self.theme_dict[row[1]['theme']].append(question_object)
+            else:
+                self.theme_dict[row[1]['theme']] = [question_object]
+
+            if row[1]['scored'] == 1:
+                self.scored_questions.append(question_object)
+
+            if row[1]['q_type'] == 's':
+                self.single_response_questions.append(question_object)
+            elif row[1]['q_type'] == 'm':
+                self.multi_response_questions.append(question_object)
+            elif row[1]['q_type'] == 't':
+                self.text_response_questions.append(question_object)
+
+        return None
 
 class Question:
 
     def __init__(self, q_row, score_indices, response_indices):
-        self.qid = q_row[0]
+        self.row = q_row
+        self.qid = q_row[1]['qid']
+        self.q_sort_int = q_row[0]
         self.q_text = q_row[1]['q_text']
+        self.q_pos_text = q_row[1]['pos_text']
         self.q_type = q_row[1]['q_type']
-        self.pos_scored = q_row[1]['pos_scored']
+        self.scored = q_row[1]['scored']
+        self.theme = q_row[1]['theme']
 
         self.score_map = self.get_score_map(q_row[1], score_indices)
         self.responses = self.get_response_map(q_row[1], response_indices)
